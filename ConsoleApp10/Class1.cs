d8be2601c0e362e628e0fa36954b2ec57fdcc30a
9045857 <9045857@mail.ru>
2022-12-09 14:49:12 +0700

    Add project files.

---
+++ ConsoleApp10/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace MyConsole
+{
+    internal class Class1
+    {
+
+        /// <summary>
+        /// Что-то пишу
+        /// </summary>
+        public static void Main() 
+        {
+            int age = 22;
+            int weight = 66;
+            double temperature = 36.66; 
+            string name = "Петр";
+            Console.WriteLine("{0,-11} = {1}", "Возраст", age);
+            Console.WriteLine("{0,-11} = {1}", "Вес", weight); 
+            Console.WriteLine("{0,-11} = {1}", "Температура", temperature); 
+            Console.WriteLine("{0,-11} = {1}", "Имя", name);
+
+        }
+    }
+}
